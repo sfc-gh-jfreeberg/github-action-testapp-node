21cf128c72691b10e9c9c501e57af53a93122092
Jason Freeberg <jason.freeberg@snowflake.com>
2023-05-15 16:34:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.json(
+    {
+      example: 'value',
+      obj: {
+        something: 'else',
+        another: 'you get the idea. Change this to change the API response',
+        number: 121323
+      }
+    });
 });
 
 app.listen(process.env.PORT, function () {
